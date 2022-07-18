0051dee459986b8e8ca7728ad88d37a987f551b0
David Adamson <david@212digital.co.uk>
2022-07-18 10:38:29 +0100

    adding SQLite connection method

--- DesktopContactsApp/NewContactWindow.xaml.cs
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -36,10 +36,12 @@ namespace DesktopContactsApp
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string databasePath = System.IO.Path.Combine(folderPath, databaseName);
 
-            SQLiteConnection connection = new SQLiteConnection(databasePath);
-            connection.CreateTable<Contact>();
-
-
+            using (SQLiteConnection connection = new SQLiteConnection(databasePath))
+            {   
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }  
+            
             Close();
 
         }
